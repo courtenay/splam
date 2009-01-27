7a69ecac2b32fdc546a4c144836f88d1062a6b1d
Court3nay <courtenay@caboo.se>
2009-01-26 21:47:16 -0800

    Heh, stupid spammers

--- lib/splam/rules/html.rb
+++ lib/splam/rules/html.rb
@@ -6,5 +6,7 @@ class Splam::Rules::Html < Splam::Rule
     
     # stupid fools!
     add_score 5 * @body.scan(/<a[^>]*><b>/).size, "<b> inside an <a>"
+    
+    add_score(200, "Entire body is an HTML tag") if @body =~ /^[<][^>]*[>]$/
   end
 end
