8aee4460efec39a10e265107b447ccf7e129f05b
Courtenay <courtenay@entp.com>
2022-07-12 15:37:32 +1200

    Cache httpbl results in REDIS global, which can be shared amongst other mitigation techniques.

--- lib/splam/rules/httpbl.rb
+++ lib/splam/rules/httpbl.rb
@@ -1,17 +1,17 @@
 require 'resolv'
-require 'system_timer' if RUBY_VERSION =~ /^1.8/
+require 'timeout'
 
 # Liberally copied from https://github.com/bpalmen/httpbl/blob/master/lib/httpbl.rb
 class Splam::Rules::Httpbl < Splam::Rule
-
-  cattr_accessor :api_key
+  class << self
+    attr_accessor :api_key
+  end
 
   def run
     return unless @request # no ip available
     return unless @request[:remote_ip] # no ip available
     
     ip = @request[:remote_ip]
-    return if ip == "127.0.0.1"
     
     if result = self.class.check_blacklist(ip)
       add_score 250, "IP address (#{ip}) appears in ProjectHoneypot blacklist. (#{result.inspect})"
@@ -19,14 +19,9 @@ class Splam::Rules::Httpbl < Splam::Rule
   end
   
   def self.check_blacklist(ip)
-    # @cache = REDIS if defined?(REDIS)
-    # result = @cache && @cache["ip.#{ip}"]
-    # result ||= resolve(ip)
-    # if @cache
-    #   @cache.set "ip.#{ip}", result if @cache
-    #   @cache.expire "ip.#{ip}", 1.week
-    # end
+    return if ip == "127.0.0.1"
     result = resolve(ip)
+    return if result == "127.0.0.1"
     response = result.split(".").collect!(&:to_i)
     
     # responses:
@@ -36,12 +31,21 @@ class Splam::Rules::Httpbl < Splam::Rule
     # c = threat score, 0..255 (0 is not threat)
     # d = type of visitor
     raise "Bad httpbl request format!" if response[0] != 127
-    return response[3] > 0 || response[2] > 100
+    if response[3] > 0 || response[2] > 100
+      return({ days: response[1], score: response[2] })
+    end
+
+    # no httpbl result, however..
+    @cache = REDIS if defined?(REDIS)
+    result = @cache && @cache["ip.#{ip}"]
+    if result
+      return({ cache: result })
+    end
   end
   
   def self.resolve(ip)
-    query = "#{@@api_key}.#{ip.split('.').reverse.join('.')}.dnsbl.httpbl.org"
-    SystemTimer::timeout(0.5) do
+    query = "#{api_key}.#{ip.split('.').reverse.join('.')}.dnsbl.httpbl.org"
+    Timeout::timeout(0.5) do
       begin
         Resolv::DNS.new.getaddress(query).to_s
       rescue Resolv::ResolvError
